9b4e769cf77700db2d1dcafb23d4fae3734aea51
vishu-tyagi <vishweshwartyagiv@gmail.com>
2024-01-29 12:29:32 -0500

    begin making combined exp results fig.

---
+++ notebooks/experiments/tms/figure/core.py
@@ -0,0 +1,13 @@
+import os
+import pickle
+import logging
+
+
+import numpy as np
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+
+logger = logging.getLogger(__name__)
+
+BUILD_DIR = "/home/vishu/repos/hbmep-paper/reports/experiments/tms/simulate_data"
+
