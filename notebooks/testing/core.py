2919b38e83b86c9e15d72bf3422f51489bf5dba1
vishu-tyagi <vishweshwartyagiv@gmail.com>
2024-02-15 12:05:28 -0500

    more tests

--- notebooks/testing/core.py
+++ notebooks/testing/core.py
@@ -7,23 +7,20 @@ import numpy as np
 import pandas as pd
 from hbmep.config import Config
 from hbmep.model import BaseModel
-import hbmep as mep
-import hbmep.functional as F
+# import hbmep as mep
+# import hbmep.functional as F
 
 from hbmep_paper.utils import setup_logging
 
 def main():
     config = Config(TOML_PATH)
-    config.FEATURES = ["participant", "participant_condition"]
-    config.BUILD_DIR = "/home/vishu/testing"
-    config.RESPONSE = config.RESPONSE[0:1]
-    # config.FEATURES = []
     setup_logging(config.BUILD_DIR, "logs")
     model = BaseModel(config)
 
-
-    df = pd.read_csv(DATA_PATH)
-    df, encoder_dict = model.load(df)
+    # df = pd.read_csv(DATA_PATH)
+    df, encoder_dict = model.load()
+    model.plot(df=df, encoder_dict=encoder_dict)
+    return
     # print(encoder_dict.keys())
     # print(type(encoder_dict.keys()))
     # print(type(encoder_dict))
