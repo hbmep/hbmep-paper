e88de65733f158f8ede9bd557bf6cc6ae497f04b
vishu-tyagi <vishweshwartyagiv@gmail.com>
2024-02-23 09:41:19 -0500

    add non-hierarchical bayesian and maximum-likelihood models

--- notebooks/experiments/tms/results__number_of_subjects.py
+++ notebooks/experiments/tms/results__number_of_subjects.py
@@ -20,7 +20,7 @@ def main():
     n_pulses = N_PULSES
     n_subjects_space = N_SUBJECTS_SPACE
 
-    draws_space = range(180)
+    draws_space = range(550)
     models = [HierarchicalBayesianModel]
 
     mae = []
