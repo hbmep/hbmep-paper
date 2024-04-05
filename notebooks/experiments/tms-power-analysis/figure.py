14822c48e0c92ef177aa36907d664c5076ae7cd0
vishu-tyagi <vishweshwartyagiv@gmail.com>
2024-04-05 15:48:14 -0400

    finish power analysis on tms data

--- notebooks/experiments/tms-power-analysis/figure.py
+++ notebooks/experiments/tms-power-analysis/figure.py
@@ -24,6 +24,71 @@ BUILD_DIR = EXPERIMENTS_DIR
 BAYESIAN_CUTOFF = .95
 FREQUENTIST_CUTOFF = .05
 
+axis_label_size = 8
+
+cmap = sns.color_palette("hls", 8)
+colors = [cmap[-1]]
+begin = 3
+colors += cmap[begin:begin + 4]
+colors = [colors[2], colors[1], colors[-2], colors[0], "k"]
+colors = [colors[-3], colors[-1]]
+
+markersize = 3
+linewidth = 1
+linestyle = "--"
+
+
+def _power_plot(ax, x, arr, models, labels):
+    assert len(x) == arr.shape[0]
+    for model_ind, model in enumerate(models):
+        match model.NAME:
+            case HierarchicalBayesianModel.NAME:
+                # y = arr[..., model_ind] > BAYESIAN_CUTOFF
+                y = arr[..., model_ind] < 0.
+            case NonHierarchicalBayesianModel.NAME:
+                y = arr[..., model_ind] < FREQUENTIST_CUTOFF
+            case _:
+                raise ValueError(f"Invalid model")
+
+        yme = y.mean(axis=-1)
+        ysem = stats.sem(y, axis=-1)
+        ax.errorbar(
+            x=x,
+            y=yme,
+            yerr=ysem,
+            marker="o",
+            label=labels[model_ind],
+            linestyle=linestyle,
+            ms=markersize,
+            linewidth=linewidth,
+            color=colors[model_ind],
+        )
+
+    ax.set_xticks(x)
+    return ax
+
+
+def _error_plot(ax, x, arr, models, labels):
+    assert len(x) == arr.shape[0]
+    for model_ind, model in enumerate(models):
+        y = arr[..., model_ind]
+        yme = y.mean(axis=-1)
+        ysem = stats.sem(y, axis=-1)
+        ax.errorbar(
+            x=x,
+            y=yme,
+            yerr=ysem,
+            marker="o",
+            label=labels[model_ind],
+            linestyle="--",
+            ms=4,
+            color=colors[model_ind],
+        )
+
+    ax.set_xticks(x)
+    ax.set_ylim(bottom=0.)
+    return ax
+
 
 def main():
     nrows, ncols = 1, 2
@@ -32,20 +97,20 @@ def main():
         ncols,
         figsize=(4.566, 2.65),
         squeeze=False,
-        constrained_layout=True
+        constrained_layout=True,
+        sharex="row"
     )
 
-    n_subjects_space = N_SUBJECTS_SPACE[1:]
-    logger.info(f"n_subjects_space: {n_subjects_space}")
     models = [
         NonHierarchicalBayesianModel,
         HierarchicalBayesianModel
     ]
     labels = [
-        "Non-Hierarchical Bayesian",
-        "Hierarchical Bayesian"
+        "Non-hierarchical Bayesian\nWilcoxon signed-rank test",
+        "Hierarchical Bayesian\n95% Highest Posterior\nDensity Interval (HPDI)\ntesting"
     ]
 
+    # With effect
     src = os.path.join(EXPERIMENTS_DIR, "mae.npy")
     mae = np.load(src)
     logger.info(f"mae: {mae.shape}")
@@ -55,33 +120,11 @@ def main():
     logger.info(f"prob: {prob.shape}")
 
     ax = axes[0, 0]
-    for model_ind, model in enumerate(models):
-        x = n_subjects_space
-        y = (
-            prob[..., model_ind] > BAYESIAN_CUTOFF
-            if model_ind == 1
-            else prob[..., model_ind] < FREQUENTIST_CUTOFF
-        )
-        yme = y.mean(axis=-1)
-        ysem = stats.sem(y, axis=-1)
-        ax.errorbar(
-            x=[model_ind / 3 + i for i in x],
-            y=yme,
-            yerr=ysem,
-            marker="o",
-            label=f"{model.NAME}",
-            linestyle="--",
-            # ms=1,
-            # linewidth=1,
-            # color=colors[model_ind]
-        )
-    ax.axhline(y=.80, linestyle="--", color="black")
-    ax.set_xticks(x)
-    ax.legend(bbox_to_anchor=(0., 1.2), loc="center", fontsize=6)
-    ax.set_yticks([.20 * i for i in range(6)])
-    ax.set_xlabel("# Subjects")
-    ax.set_ylabel("Power (β)")
+    ax = _power_plot(ax, N_SUBJECTS_SPACE[1:], prob, models, labels)
+    ax.axhline(y=.8, linestyle="--", color="r", linewidth=1, xmax=.99)
+    ax.set_yticks([.2 * i for i in range(6)])
 
+    # Without effect
     src = os.path.join(EXPERIMENTS_NO_EFFECT_DIR, "mae.npy")
     mae = np.load(src)
     logger.info(f"mae: {mae.shape}")
@@ -91,143 +134,64 @@ def main():
     logger.info(f"prob: {prob.shape}")
 
     ax = axes[0, 1]
-    for model_ind, model in enumerate(models):
-        x = n_subjects_space
-        y = (
-            prob[..., model_ind] > BAYESIAN_CUTOFF
-            if model_ind == 1
-            else prob[..., model_ind] < FREQUENTIST_CUTOFF
-        )
-        yme = y.mean(axis=-1)
-        ysem = stats.sem(y, axis=-1)
-        ax.errorbar(
-            x=[model_ind / 3 + i for i in x],
-            y=yme,
-            yerr=2 * ysem,
-            marker="o",
-            label=f"{model.NAME}",
-            linestyle="--",
-            # ms=1,
-            # linewidth=1,
-            # color=colors[model_ind]
-        )
-    ax.axhline(y=.05, linestyle="--", color="black")
-    ax.set_xticks(x)
-    ax.legend(bbox_to_anchor=(0., 1.2), loc="center", fontsize=6)
-    ax.set_yticks([.02 * i for i in range(6)])
-    ax.set_xlabel("# Subjects")
-    ax.set_ylabel("Power (β)")
+    ax = _power_plot(ax, N_SUBJECTS_SPACE[1:], prob, models, labels)
+    ax.axhline(y=.05, linestyle="--", color="r", linewidth=1, xmax=.99)
+    ax.set_yticks([.02 * i for i in range(3)] + [.05])
+    ax.set_ylim(top=.104)
+    # ax.set_yticks([.04 * i for i in range(3)] + [.05])
 
-    dest = os.path.join(EXPERIMENTS_DIR, "results.png")
-    fig.savefig(dest, dpi=600)
-    logger.info(f"Saved to {dest}")
+    for i in range(nrows):
+        for j in range(ncols):
+            ax = axes[i, j]
+            ax.set_xlabel("Number of participants", fontsize=axis_label_size)
+            ax.set_ylabel("")
+            if not i:
+                if not j: ax.set_ylabel("True positive rate (1 - β)", fontsize=axis_label_size)
+                if j: ax.set_ylabel("False positive rate (α)", fontsize=axis_label_size)
+            else:
+                ax.set_ylabel("Mean Absolute Error", fontsize=axis_label_size)
 
-    # ax = axes[0, 0]
-    # for model_ind, model in enumerate(models):
-    #     # if model_ind == 3: continue
-    #     x = n_subjects_space
-    #     # Jitter x
-    #     # x = [model_ind / 100 + i for i in x]
-    #     y = mae[..., model_ind]
-    #     yme = y.mean(axis=-1)
-    #     ysem = stats.sem(y, axis=-1)
-    #     # ysd = y.std(axis=-1)
-    #     ax.errorbar(
-    #         x=x,
-    #         y=yme,
-    #         yerr=ysem,
-    #         marker="o",
-    #         label=labels[model_ind],
-    #         linestyle="--",
-    #         ms=4,
-    #         # linewidth=1,
-    #         color=colors[model_ind]
-    #     )
-    #     ax.set_xticks(x)
-    #     ax.set_xlabel("# Subjects")
-    #     ax.set_ylabel("MAE")
+            sides = ["top", "right"]
+            for side in sides:
+                ax.spines[side].set_visible(False)
+            ax.tick_params(
+                axis='both',
+                which='both',
+                left=True,
+                bottom=True,
+                right=False,
+                top=False,
+                labelleft=True,
+                labelbottom=True,
+                labelright=False,
+                labeltop=False,
+                labelrotation=15,
+                labelsize=8
+            )
+            ax.grid(axis="y", linestyle="--", alpha=.25)
 
-    # ax.set_ylim(bottom=0.)
-    # ax.set_xlabel("Number of Participants", fontsize=axis_label_size)
-    # ax.set_ylabel("MAE on Threshold $($% MSO$)$", fontsize=axis_label_size)
+    Hypothesis = "Hypothesis"
+    fig.suptitle(r"$\bf{" + Hypothesis + "}$" + ": $\Delta \sim \mathcal{N}(\mu_{\Delta}, \sigma_{\Delta})$, H0: $\mu_{\Delta} \leq 0$ vs H1: $\mu_{\Delta} > 0$", fontsize=axis_label_size)
+    ax = axes[0, 0]
+    ax.text(1.4, .805, "80% Power", va="bottom", ha="left", fontsize=7)
+    ax.set_title("Null hypothesis is false, $\Delta \sim \mathcal{N}(3, 1.5)$", fontsize=axis_label_size - 1)
 
-    # n_pulses_space = N_PULSES_SPACE
-    # src = os.path.join(NUMBER_OF_PULSES_DIR, "mae.npy")
-    # mae = np.load(src)
+    ax = axes[0, 1]
+    ax.text(1.4, .051, "0.05 Significance level", va="bottom", ha="left", fontsize=7)
+    ax.set_title("Null hypothesis is true, $\Delta \sim \mathcal{N}(0, 1.5)$", fontsize=axis_label_size - 1)
+    # ax.legend(loc="upper left", fontsize=6)
 
-    # ax = axes[0, 1]
-    # for model_ind, model in enumerate(models):
-    #     # if model_ind == 3: continue
-    #     x = n_pulses_space
-    #     # Jitter x
-    #     # x = [model_ind / 100 + i for i in x]
-    #     y = mae[..., model_ind]
-    #     yme = y.mean(axis=-1)
-    #     ysem = stats.sem(y, axis=-1)
-    #     # ysd = y.std(axis=-1)
-    #     ax.errorbar(
-    #         x=x,
-    #         y=yme,
-    #         yerr=ysem,
-    #         marker="o",
-    #         label=labels[model_ind],
-    #         linestyle="--",
-    #         ms=4,
-    #         # linewidth=1,
-    #         color=colors[model_ind]
-    #     )
-    #     ax.set_xticks(x)
-    #     # ax.legend(bbox_to_anchor=(0., 1.2), loc="center", fontsize=6)
-    #     ax.set_xlabel("# Pulses")
-    #     ax.set_ylabel("MAE")
-
-    # ax.set_ylim(bottom=0.)
-    # ax.set_xlabel("Number of Intensities", fontsize=axis_label_size)
-    # ax.set_ylabel("MAE on Threshold $($% MSO$)$", fontsize=axis_label_size)
-
-    # for i in range(ncols):
-    #     ax = axes[0, i]
-    #     sides = ["top", "right"]
-    #     for side in sides:
-    #         ax.spines[side].set_visible(False)
-    #     ax.tick_params(
-    #         axis='both',
-    #         which='both',
-    #         left=True,
-    #         bottom=True,
-    #         right=False,
-    #         top=False,
-    #         labelleft=True,
-    #         labelbottom=True,
-    #         labelright=False,
-    #         labeltop=False,
-    #         labelrotation=15,
-    #         labelsize=8
-    #     )
-    #     ax.grid(axis="y", linestyle="--", alpha=.25)
-    #     ax.set_ylabel("")
-
-    # ax = axes[0, 0]
-    # ax.sharey(axes[0, 1])
-    # ax.set_ylabel("Mean Absolute Error on Threshold\n$($% MSO$)$", fontsize=axis_label_size)
-    # ax.legend(loc="upper right", fontsize=6)
-    # handles, labels = ax.get_legend_handles_labels()
-    # ax.get_legend().remove()
-    # ax.legend(handles[:3], labels[:3], loc="upper right", fontsize=6, frameon=True)
-
-    # ax = axes[0, 1]
-    # ax.tick_params(labelleft=False)
-    # ax.legend(handles[3:], labels[3:], loc="upper right", fontsize=6, frameon=True)
-    # ax.set_ylim(top=10.5)
+    ax = axes[0, 1]
+    ax.legend(loc="upper center", fontsize=6.5)
 
     fig.align_xlabels()
     fig.align_ylabels()
 
-    dest = os.path.join(BUILD_DIR, "results.svg")
+    dest = os.path.join(BUILD_DIR, "power.svg")
     fig.savefig(dest, dpi=600)
     logger.info(f"Saved to {dest}")
 
-    dest = os.path.join(BUILD_DIR, "results.png")
+    dest = os.path.join(BUILD_DIR, "power.png")
     fig.savefig(dest, dpi=600)
     logger.info(f"Saved to {dest}")
 
