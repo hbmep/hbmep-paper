7540dad26eb434adc94142dc49c96137e277764b
vishu-tyagi <vishweshwartyagiv@gmail.com>
2024-05-01 10:46:55 -0400

    update figure

--- notebooks/experiments/tms-power-analysis/figure.py
+++ notebooks/experiments/tms-power-analysis/figure.py
@@ -4,7 +4,6 @@ import logging
 import numpy as np
 import scipy.stats as stats
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 from hbmep_paper.utils import setup_logging
 from models import (
@@ -23,19 +22,11 @@ logger = logging.getLogger(__name__)
 plt.rcParams["svg.fonttype"] = "none"
 
 BUILD_DIR = EXPERIMENTS_DIR
-BAYESIAN_CUTOFF = .95
 FREQUENTIST_CUTOFF = .05
 
 axis_label_size = 12
 inside_text_size = 8
 
-# cmap = sns.color_palette("hls", 8)
-# colors = [cmap[-1]]
-# begin = 3
-# colors += cmap[begin:begin + 4]
-# colors = [colors[2], colors[1], colors[-2], colors[0], "k"]
-# colors = [colors[-4], colors[-3], colors[-2], colors[-1]]
-# colors = ["#00ced1", "#ffa500", "#0000ff", "#ff1493", "k"]
 colors = ["#00ced1", "#ffa500", "#0000ff", "k"]
 IF_SKIP = True
 IF_SKIP = not IF_SKIP
@@ -52,7 +43,6 @@ def _power_plot(ax, x, arr, models, labels):
             continue
         match model.NAME:
             case HierarchicalBayesianModel.NAME:
-                # y = arr[..., model_ind] > BAYESIAN_CUTOFF
                 y = arr[..., model_ind] < 0.
             case NonHierarchicalBayesianModel.NAME | MaximumLikelihoodModel.NAME | NelderMeadOptimization.NAME:
                 y = arr[..., model_ind] < FREQUENTIST_CUTOFF
@@ -77,28 +67,6 @@ def _power_plot(ax, x, arr, models, labels):
     return ax
 
 
-def _error_plot(ax, x, arr, models, labels):
-    assert len(x) == arr.shape[0]
-    for model_ind, model in enumerate(models):
-        y = arr[..., model_ind]
-        yme = y.mean(axis=-1)
-        ysem = stats.sem(y, axis=-1)
-        ax.errorbar(
-            x=x,
-            y=yme,
-            yerr=ysem,
-            marker="o",
-            label=labels[model_ind],
-            linestyle="--",
-            ms=4,
-            color=colors[model_ind],
-        )
-
-    ax.set_xticks(x)
-    ax.set_ylim(bottom=0.)
-    return ax
-
-
 def main():
     const = 1.25
     nrows, ncols = 1, 2
@@ -117,16 +85,10 @@ def main():
         NonHierarchicalBayesianModel,
         HierarchicalBayesianModel,
     ]
-    # labels = [
-    #     "Nelder-Mead method\nWilcoxon signed-rank test",
-    #     "Maximum likelihood estimation\nWilcoxon signed-rank test",
-    #     "Non-hierarchical Bayesian\nWilcoxon signed-rank test",
-    #     "Hierarchical Bayesian\n95% Highest density interval\n(HDI) testing",
-    # ]
     labels = [
-        "Nelder-Mead method\nSigned-rank test",
-        "Maximum likelihood estimation\nSigned-rank test",
-        "Non-hierarchical Bayesian\nWilcoxon one-sided\nsigned-rank test",
+        "Nelder-Mead method\nOne-sided signed-rank test",
+        "Maximum likelihood estimation\nOne-sided signed-rank test",
+        "Non-hierarchical Bayesian\nOne-sided signed-rank test",
         "Hierarchical Bayesian\n95% highest density interval\n(HDI) testing",
     ]
 
@@ -159,7 +121,6 @@ def main():
     ax.axhline(y=.05, linestyle="--", color="r", linewidth=1, xmax=.99)
     ax.set_yticks([.02 * i for i in range(6)] + [.05])
     ax.set_ylim(top=.11)
-    # ax.set_yticks([.04 * i for i in range(3)] + [.05])
 
     for i in range(nrows):
         for j in range(ncols):
@@ -191,22 +152,11 @@ def main():
             )
             ax.grid(axis="y", linestyle="--", alpha=.25)
 
-    Hypothesis = "Hypothesis"
-    # fig.suptitle(r"$\bf{" + Hypothesis + "}$" + ": $\Delta \sim \mathcal{N}(\mu_{\Delta}, \sigma_{\Delta})$, H0: $\mu_{\Delta} \leq 0$ vs H1: $\mu_{\Delta} > 0$", fontsize=axis_label_size)
     ax = axes[0, 0]
     ax.text(2, .805, "80% Power", va="bottom", ha="left", fontsize=inside_text_size)
-    # ax.set_title("Null hypothesis is false, $\Delta \sim \mathcal{N}(3, 1.5)$", fontsize=axis_label_size - 1)
 
     ax = axes[0, 1]
     ax.text(2, .051, "5% Significance level", va="bottom", ha="left", fontsize=inside_text_size)
-    # ax.set_title("Null hypothesis is true, $\Delta \sim \mathcal{N}(0, 1.5)$", fontsize=axis_label_size - 1)
-    # ax.legend(loc="upper left", fontsize=6)
-    # ax.annotate(
-    #     r"$\}$",
-    #     fontsize=32,
-    #     xy=(0.94, 0.8),
-    #     xycoords='figure fraction'
-    # )
 
     ax = axes[0, 1]
     ax.legend(loc="upper right", fontsize=inside_text_size, labelspacing=1.1)
