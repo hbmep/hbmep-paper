453a79ed570dd5475d432aee2cc127692b324569
vishu-tyagi <vishweshwartyagiv@gmail.com>
2024-06-07 20:21:03 -0400

    remove rat analysis

--- notebooks/cross-validation-rms-opt/rats/constants.py
+++
@@ -1,7 +0,0 @@
-TOML_PATH = "/home/vishu/repos/hbmep-paper/configs/cross-validation/J_RCML_000.toml"
-DATA_PATH = "/home/vishu/data/hbmep-processed/J_RCML_000/data.csv"
-
-INFERENCE_FILE = "inference.pkl"
-NETCODE_FILE = "inference_data.nc"
-
-BUILD_DIR = "/home/vishu/repos/hbmep-paper/reports/cross-validation-rms-opt/rats"
