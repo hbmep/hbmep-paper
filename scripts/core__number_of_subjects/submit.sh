bb3d0b81cde27e9e3b3c812e3de50f50d7550ff6
vishu-tyagi <vishweshwartyagiv@gmail.com>
2024-06-07 20:49:26 -0400

    fix typo

--- scripts/core__number_of_subjects/submit.sh
+++ scripts/core__number_of_subjects/submit.sh
@@ -5,5 +5,5 @@ START=0
 END=1800
 
 for (( COUNTER=START; COUNTER<=END; COUNTER+=STEP )); do
-    sbatch -c 64 --mem=258GB core__power.sh $COUNTER $((COUNTER+200))
+    sbatch -c 64 --mem=258GB core.sh $COUNTER $((COUNTER+200))
 done
