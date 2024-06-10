058a1918c9669f0b6b4f80f336659c1b3eb3bdd4
vishu-tyagi <vishweshwartyagiv@gmail.com>
2024-06-10 07:13:36 -0400

    add slurm script to run pulses experiment

---
+++ scripts/core__number_of_pulses/core.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+REPO="$HOME/repos/hbmep-paper"
+PYTHON_ENV="$REPO/.venv/bin/activate"
+
+# Change to the directory where the script is located
+cd $REPO/notebooks/simulations/
+
+# Activate the virtual environment
+source $PYTHON_ENV
+
+# Run the script
+python -m core__number_of_pulses $1 $2
