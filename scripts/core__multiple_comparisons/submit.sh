a45c7638b26627f5168e36a25aed215f5fca34d2
vishu-tyagi <vishweshwartyagiv@gmail.com>
2024-07-19 16:41:54 -0400

    add script to run multiple comparisons on HPC

---
+++ scripts/core__multiple_comparisons/submit.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+STEP=50
+START=0
+END=$((2000-$STEP))
+
+for (( COUNTER=START; COUNTER<=END; COUNTER+=STEP )); do
+    sbatch -c 32 --mem=64GB core.sh $COUNTER $((COUNTER+STEP))
+done
