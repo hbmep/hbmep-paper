6f0b7788f8d16ac27c4c155f262bdec1437c918f
vishu-tyagi <vishweshwartyagiv@gmail.com>
2024-07-26 14:15:08 -0400

    update with effect simulation

--- notebooks/simulations-multiple-comparisons/utils.py
+++ notebooks/simulations-multiple-comparisons/utils.py
@@ -13,13 +13,8 @@ def _generate_simulation_data_dirs():
             os.path.join(BUILD_DIR, "with_no_effect")
         ),
         "with_effect": (
-            np.array([5] * 4),
+            np.array([3, 4, 5, 6]),
             np.array([2.5] * 4),
             os.path.join(BUILD_DIR, "with_effect")
-        ),
-        "test": (
-            np.array([0, -2, 4, 0.5]),
-            np.array([2.5] * 4),
-            os.path.join(BUILD_DIR, "test")
-        ),
+        )
     }
