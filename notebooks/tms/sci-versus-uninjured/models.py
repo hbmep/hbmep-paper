dde5c82304b5c5ec9be8fd8e38649c418ba25046
vishu-tyagi <vishweshwartyagiv@gmail.com>
2024-08-20 12:42:39 -0400

    add non-hierarchical Bayesian model

--- notebooks/tms/sci-versus-uninjured/models.py
+++ notebooks/tms/sci-versus-uninjured/models.py
@@ -8,6 +8,11 @@ from hbmep.nn import functional as F
 from hbmep.model import GammaModel
 from hbmep.model.utils import Site as site
 
+from hbmep_paper.models import (
+    NonHierarchicalBaseModel,
+    ConstrainedOptimization
+)
+
 
 class HierarchicalBayesianModel(GammaModel):
     NAME = "hierarchical_bayesian_model"
@@ -146,5 +151,111 @@ class HierarchicalBayesianModel(GammaModel):
                 numpyro.sample(
                     site.obs,
                     Mixture,
+                    obs=response_obs
+                )
+
+
+class NonHierarchicalBayesianModel(NonHierarchicalBaseModel, GammaModel):
+    NAME = "non_hierarchical_bayesian_model"
+
+    def __init__(self, config: Config):
+        super(NonHierarchicalBayesianModel, self).__init__(config=config)
+        self.n_jobs = -1
+
+    def _model(self, intensity, features, response_obs=None):
+        n_data = intensity.shape[0]
+        n_features = np.max(features, axis=0) + 1
+        feature0 = features[..., 0]
+        feature1 = features[..., 1]
+
+        with numpyro.plate(site.n_response, self.n_response):
+            with numpyro.plate(site.n_features[1], n_features[1]):
+                with numpyro.plate(site.n_features[0], n_features[0]):
+                    # Hyper-priors
+                    a_loc = numpyro.sample(
+                        "a_loc", dist.TruncatedNormal(50., 50., low=0)
+                    )
+                    a_scale = numpyro.sample("a_scale", dist.HalfNormal(50.))
+
+                    b_scale = numpyro.sample("b_scale", dist.HalfNormal(5.))
+
+                    L_scale = numpyro.sample("L_scale", dist.HalfNormal(.5))
+                    ell_scale = numpyro.sample("ell_scale", dist.HalfNormal(10.))
+                    H_scale = numpyro.sample("H_scale", dist.HalfNormal(5.))
+
+                    c_1_scale = numpyro.sample("c_1_scale", dist.HalfNormal(5.))
+                    c_2_scale = numpyro.sample("c_2_scale", dist.HalfNormal(5.))
+
+                    # Priors
+                    a = numpyro.sample(
+                        site.a, dist.TruncatedNormal(a_loc, a_scale, low=0)
+                    )
+
+                    b = numpyro.sample(site.b, dist.HalfNormal(scale=b_scale))
+
+                    L = numpyro.sample(site.L, dist.HalfNormal(scale=L_scale))
+                    ell = numpyro.sample(site.ell, dist.HalfNormal(scale=ell_scale))
+                    H = numpyro.sample(site.H, dist.HalfNormal(scale=H_scale))
+
+                    c_1 = numpyro.sample(site.c_1, dist.HalfCauchy(scale=c_1_scale))
+                    c_2 = numpyro.sample(site.c_2, dist.HalfCauchy(scale=c_2_scale))
+
+        # # Outlier Distribution
+        # outlier_prob = numpyro.sample(site.outlier_prob, dist.Uniform(0., .01))
+        # outlier_scale = numpyro.sample(site.outlier_scale, dist.HalfNormal(10))
+
+        with numpyro.plate(site.n_response, self.n_response):
+            with numpyro.plate(site.n_data, n_data):
+                # Model
+                mu = numpyro.deterministic(
+                    site.mu,
+                    F.rectified_logistic(
+                        x=intensity,
+                        a=a[feature0, feature1],
+                        b=b[feature0, feature1],
+                        L=L[feature0, feature1],
+                        ell=ell[feature0, feature1],
+                        H=H[feature0, feature1]
+                    )
+                )
+                beta = numpyro.deterministic(
+                    site.beta,
+                    self.rate(
+                        mu,
+                        c_1[feature0, feature1],
+                        c_2[feature0, feature1]
+                    )
+                )
+                alpha = numpyro.deterministic(
+                    site.alpha,
+                    self.concentration(mu, beta)
+                )
+
+                # # Mixture
+                # q = numpyro.deterministic(
+                #     site.q, outlier_prob * jnp.ones((n_data, self.n_response))
+                # )
+                # bg_scale = numpyro.deterministic(
+                #     site.bg_scale,
+                #     outlier_scale * jnp.ones((n_data, self.n_response))
+                # )
+
+                # mixing_distribution = dist.Categorical(
+                #     probs=jnp.stack([1 - q, q], axis=-1)
+                # )
+                # component_distributions=[
+                #     dist.Gamma(concentration=alpha, rate=beta),
+                #     dist.HalfNormal(scale=bg_scale)
+                # ]
+
+                # Mixture = dist.MixtureGeneral(
+                #     mixing_distribution=mixing_distribution,
+                #     component_distributions=component_distributions
+                # )
+
+                # Observation
+                numpyro.sample(
+                    site.obs,
+                    dist.Gamma(concentration=alpha, rate=beta),
                     obs=response_obs
                 )
