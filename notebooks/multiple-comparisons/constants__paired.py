0c41768b999aceacc0ae7c2c1f099e1d0747fdb1
vishu-tyagi <vishweshwartyagiv@gmail.com>
2024-09-03 13:19:26 -0400

    update paths

--- notebooks/multiple-comparisons/constants__paired.py
+++ notebooks/multiple-comparisons/constants__paired.py
@@ -4,12 +4,13 @@ import os
 TOML_PATH = "/home/vishu/repos/hbmep-paper/configs/multiple-comparisons/config.toml"
 DATA_PATH = "/home/vishu/data/hbmep-processed/human/tms/proc_2024-06-08.csv"
 
-BUILD_DIR = "/home/vishu/repos/hbmep-paper/reports/multiple-comparisons"
-LEARN_POSTERIOR_DIR = os.path.join(BUILD_DIR, "learn_posterior")
-EXPERIMENTS_DIR = os.path.join(BUILD_DIR, "paired_experiments")
+BUILD_DIR_PARENT = "/home/vishu/repos/hbmep-paper/reports/multiple-comparisons"
+LEARN_POSTERIOR_DIR = os.path.join(BUILD_DIR_PARENT, "learn_posterior")
+
+BUILD_DIR = os.path.join(BUILD_DIR_PARENT, "paired")
 
 TOTAL_SUBJECTS = 32
-N_SUBJECTS_SPACE = [2, 4, 6, 8, 12, 16]
+N_SUBJECTS_SPACE = [2, 4, 8, 12, 16]
 
 TOTAL_PULSES = 48
 TOTAL_REPS = 1
