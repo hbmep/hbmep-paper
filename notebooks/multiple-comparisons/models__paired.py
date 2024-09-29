57a0ec9a1745c2aa7672238b5a63fb448bdd9d3b
vishu-tyagi <vishweshwartyagiv@gmail.com>
2024-09-29 08:22:37 -0400

    add nHB model

--- notebooks/multiple-comparisons/models__paired.py
+++ notebooks/multiple-comparisons/models__paired.py
@@ -376,83 +376,82 @@ class HierarchicalBayesianModel(GammaModel):
                 )
 
 
-# class NonHierarchicalBayesianModel(NonHierarchicalBaseModel, GammaModel):
-#     NAME = "non_hierarchical_bayesian_model"
+class NonHierarchicalBayesianModel(NonHierarchicalBaseModel, GammaModel):
+    NAME = "non_hierarchical_bayesian_model"
 
-#     def __init__(self, config: Config):
-#         super(NonHierarchicalBayesianModel, self).__init__(config=config)
-#         self.n_jobs = -1
+    def __init__(self, config: Config):
+        super(NonHierarchicalBayesianModel, self).__init__(config=config)
+        self.n_jobs = -1
 
-#     def _model(self, intensity, features, response_obs=None):
-#         n_data = intensity.shape[0]
-#         n_features = np.max(features, axis=0) + 1
-#         feature0 = features[..., 0]
-#         feature1 = features[..., 1]
+    def _model(self, intensity, features, response_obs=None):
+        n_data = intensity.shape[0]
+        n_features = np.max(features, axis=0) + 1
+        feature0 = features[..., 0]
+        feature1 = features[..., 1]
 
-#         with numpyro.plate(site.n_response, self.n_response):
-#             with numpyro.plate(site.n_features[1], n_features[1]):
-#                 with numpyro.plate(site.n_features[0], n_features[0]):
-#                     # Hyper-priors
-#                     a_loc = numpyro.sample(
-#                         "a_loc", dist.TruncatedNormal(50., 50., low=0)
-#                     )
-#                     a_scale = numpyro.sample("a_scale", dist.HalfNormal(50.))
+        with numpyro.plate(site.n_response, self.n_response):
+            with numpyro.plate(site.n_features[1], n_features[1]):
+                with numpyro.plate(site.n_features[0], n_features[0]):
+                    # Hyper-priors
+                    a_loc = numpyro.sample("a_loc", dist.TruncatedNormal(50., 30., low=0))
+                    a_scale = numpyro.sample("a_scale", dist.HalfNormal(30.))
 
-#                     b_scale = numpyro.sample("b_scale", dist.HalfNormal(5.))
+                    b_scale = numpyro.sample("b_scale", dist.HalfNormal(1.))
 
-#                     L_scale = numpyro.sample("L_scale", dist.HalfNormal(.5))
-#                     ell_scale = numpyro.sample("ell_scale", dist.HalfNormal(10.))
-#                     H_scale = numpyro.sample("H_scale", dist.HalfNormal(5.))
+                    L_scale = numpyro.sample("L_scale", dist.HalfNormal(.1))
+                    ell_scale = numpyro.sample("ell_scale", dist.HalfNormal(1.))
+                    H_scale = numpyro.sample("H_scale", dist.HalfNormal(5.))
 
-#                     c_1_scale = numpyro.sample("c_1_scale", dist.HalfNormal(5.))
-#                     c_2_scale = numpyro.sample("c_2_scale", dist.HalfNormal(5.))
+                    c_1_scale = numpyro.sample("c_1_scale", dist.HalfNormal(5.))
+                    c_2_scale = numpyro.sample("c_2_scale", dist.HalfNormal(.5))
 
-#                     # Priors
-#                     a = numpyro.sample(
-#                         site.a, dist.TruncatedNormal(a_loc, a_scale, low=0)
-#                     )
-#                     b = numpyro.sample(site.b, dist.HalfNormal(b_scale))
+                    # Priors
+                    a = numpyro.sample(
+                        site.a, dist.TruncatedNormal(a_loc, a_scale, low=0)
+                    )
+                    b = numpyro.sample(site.b, dist.HalfNormal(b_scale))
 
-#                     L = numpyro.sample(site.L, dist.HalfNormal(L_scale))
-#                     ell = numpyro.sample(site.ell, dist.HalfNormal(ell_scale))
-#                     H = numpyro.sample(site.H, dist.HalfNormal(H_scale))
+                    L = numpyro.sample(site.L, dist.HalfNormal(L_scale))
+                    ell = numpyro.sample(site.ell, dist.HalfNormal(ell_scale))
+                    H = numpyro.sample(site.H, dist.HalfNormal(H_scale))
 
-#                     c_1 = numpyro.sample(site.c_1, dist.HalfNormal(c_1_scale))
-#                     c_2 = numpyro.sample(site.c_2, dist.HalfNormal(c_2_scale))
+                    c_1 = numpyro.sample(site.c_1, dist.HalfNormal(c_1_scale))
+                    c_2 = numpyro.sample(site.c_2, dist.HalfNormal(c_2_scale))
 
-#         with numpyro.plate(site.n_response, self.n_response):
-#             with numpyro.plate(site.n_data, n_data):
-#                 # Model
-#                 mu = numpyro.deterministic(
-#                     site.mu,
-#                     F.rectified_logistic(
-#                         x=intensity,
-#                         a=a[feature0, feature1],
-#                         b=b[feature0, feature1],
-#                         L=L[feature0, feature1],
-#                         ell=ell[feature0, feature1],
-#                         H=H[feature0, feature1]
-#                     )
-#                 )
-#                 beta = numpyro.deterministic(
-#                     site.beta,
-#                     self.rate(
-#                         mu,
-#                         c_1[feature0, feature1],
-#                         c_2[feature0, feature1]
-#                     )
-#                 )
-#                 alpha = numpyro.deterministic(
-#                     site.alpha,
-#                     self.concentration(mu, beta)
-#                 )
+        with numpyro.plate(site.n_response, self.n_response):
+            with numpyro.plate(site.n_data, n_data):
+                # Model
+                mu = numpyro.deterministic(
+                    site.mu,
+                    S.rectified_logistic(
+                        x=intensity,
+                        a=a[feature0, feature1],
+                        b=b[feature0, feature1],
+                        L=L[feature0, feature1],
+                        ell=ell[feature0, feature1],
+                        H=H[feature0, feature1],
+                        eps=EPS
+                    )
+                )
+                beta = numpyro.deterministic(
+                    site.beta,
+                    self.rate(
+                        mu,
+                        c_1[feature0, feature1],
+                        c_2[feature0, feature1]
+                    )
+                )
+                alpha = numpyro.deterministic(
+                    site.alpha,
+                    self.concentration(mu, beta)
+                )
 
-#                 # Observation
-#                 numpyro.sample(
-#                     site.obs,
-#                     dist.Gamma(concentration=alpha, rate=beta),
-#                     obs=response_obs
-#                 )
+                # Observation
+                numpyro.sample(
+                    site.obs,
+                    dist.Gamma(concentration=alpha, rate=beta),
+                    obs=response_obs
+                )
 
 
 # class MaximumLikelihoodModel(NonHierarchicalBaseModel, GammaModel):
