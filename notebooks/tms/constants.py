4eb66da077466abf849dababcee824f12e5373d2
vishu-tyagi <vishweshwartyagiv@gmail.com>
2024-10-16 17:09:35 -0400

    update paths

--- notebooks/tms/constants.py
+++ notebooks/tms/constants.py
@@ -10,7 +10,7 @@ NETCODE_FILE = "inference_data.nc"
 BUILD_DIR = "/home/vishu/repos/hbmep-paper/reports/tms/"
 
 BOOTSTRAP_DIR = os.path.join(BUILD_DIR, "bootstrap")
-BOOTSTRAP_EXPERIMENTS_DIR = os.path.join(BOOTSTRAP_DIR, "experiments")
+BOOTSTRAP_EXPERIMENTS_DIR = os.path.join(BOOTSTRAP_DIR, "experiments_with_effect")
 BOOTSTRAP_EXPERIMENTS_NO_EFFECT_DIR = os.path.join(BOOTSTRAP_DIR, "experiments_no_effect")
 
 NUM_BOOTSTRAPS = 4000
