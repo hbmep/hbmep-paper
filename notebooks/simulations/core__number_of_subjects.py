e1609dacebccb6e8f202634e9c9d536cc7f905a2
vishu-tyagi <vishweshwartyagiv@gmail.com>
2024-11-03 14:51:37 -0500

    add NM

--- notebooks/simulations/core__number_of_subjects.py
+++ notebooks/simulations/core__number_of_subjects.py
@@ -86,7 +86,7 @@ def main(draws_space, n_subjects_space, models, n_jobs=-1):
             case (
                 HierarchicalBayesianModel.NAME
                 | NonHierarchicalBayesianModel.NAME
-                | MaximumLikelihoodModel.NAME
+                # | MaximumLikelihoodModel.NAME
             ):
                 # Load data
                 ind = (
@@ -151,7 +151,7 @@ def main(draws_space, n_subjects_space, models, n_jobs=-1):
                 del a_true, a_pred
                 gc.collect()
 
-            case NelderMeadOptimization.NAME:
+            case NelderMeadOptimization.NAME | MaximumLikelihoodModel.NAME:
                 # Load data
                 ind = (
                     (simulation_df[simulator.features[0]] < n_subjects) &
@@ -245,12 +245,12 @@ if __name__ == "__main__":
     ## Uncomment the following to run
     ## experiment for different models
 
-    # Run hierarchical models
-    n_jobs = -1
-    n_subjects_space = N_SUBJECTS_SPACE
-    models = [
-        HierarchicalBayesianModel
-    ]
+    # # Run hierarchical models
+    # n_jobs = -1
+    # n_subjects_space = N_SUBJECTS_SPACE
+    # models = [
+    #     HierarchicalBayesianModel
+    # ]
 
     # # Run non-hierarchical models including
     # # non-hierarchical Bayesian and Maximum Likelihood
@@ -261,10 +261,11 @@ if __name__ == "__main__":
     #     # MaximumLikelihoodModel
     # ]
 
-    # # Run non-hierarchical Nelder-Mead optimization
-    # n_jobs = -1
-    # n_subjects_space = N_SUBJECTS_SPACE[-1:]
+    # Run non-hierarchical Nelder-Mead optimization
+    n_jobs = -1
+    n_subjects_space = N_SUBJECTS_SPACE[-1:]
     # models = [NelderMeadOptimization]
+    models = [MaximumLikelihoodModel]
 
     main(
         draws_space=draws_space,
--- notebooks/simulations/models__accuracy.py
+++ notebooks/simulations/models__accuracy.py
@@ -172,68 +172,96 @@ class NonHierarchicalBayesianModel(NonHierarchicalBaseModel, GammaModel):
                 )
 
 
-class MaximumLikelihoodModel(NonHierarchicalBaseModel, GammaModel):
+# class MaximumLikelihoodModel(NonHierarchicalBaseModel, GammaModel):
+#     NAME = "maximum_likelihood_model"
+
+#     def __init__(self, config: Config):
+#         super(MaximumLikelihoodModel, self).__init__(config=config)
+#         self.n_jobs = -1
+
+#     def _model(self, intensity, features, response_obs=None):
+#         n_data = intensity.shape[0]
+#         n_features = np.max(features, axis=0) + 1
+#         feature0 = features[..., 0]
+
+#         with numpyro.plate(site.n_response, self.n_response):
+#             with numpyro.plate(site.n_features[0], n_features[0]):
+#                 # Uniform priors (maximum likelihood estimation)
+#                 a = numpyro.sample(
+#                     site.a, dist.Uniform(0., 150.)
+#                 )
+
+#                 b = numpyro.sample(site.b, dist.Uniform(0., 10.))
+
+#                 L = numpyro.sample(site.L, dist.Uniform(0., 10.))
+#                 ell = numpyro.sample(site.ell, dist.Uniform(0., 10.))
+#                 H = numpyro.sample(site.H, dist.Uniform(0., 10.))
+
+#                 c_1 = numpyro.sample(site.c_1, dist.Uniform(0., 10.))
+#                 c_2 = numpyro.sample(site.c_2, dist.Uniform(0., 10.))
+
+#         with numpyro.plate(site.n_response, self.n_response):
+#             with numpyro.plate(site.n_data, n_data):
+#                 # Model
+#                 mu = numpyro.deterministic(
+#                     site.mu,
+#                     S.rectified_logistic(
+#                         x=intensity,
+#                         a=a[feature0],
+#                         b=b[feature0],
+#                         L=L[feature0],
+#                         ell=ell[feature0],
+#                         H=H[feature0],
+#                         eps=EPS
+#                     )
+#                 )
+#                 beta = numpyro.deterministic(
+#                     site.beta,
+#                     self.rate(
+#                         mu,
+#                         c_1[feature0],
+#                         c_2[feature0]
+#                     )
+#                 )
+#                 alpha = numpyro.deterministic(
+#                     site.alpha,
+#                     self.concentration(mu, beta)
+#                 )
+
+#                 # Observation
+#                 numpyro.sample(
+#                     site.obs,
+#                     dist.Gamma(concentration=alpha, rate=beta),
+#                     obs=response_obs
+#                 )
+
+
+
+class MaximumLikelihoodModel(BoundConstrainedOptimization, GammaModel):
     NAME = "maximum_likelihood_model"
 
     def __init__(self, config: Config):
         super(MaximumLikelihoodModel, self).__init__(config=config)
+        # Required
+        self.method = "Nelder-Mead"
+        self.named_args = [site.a, site.b, site.L, site.ell, site.H, site.c_1, site.c_2]
+        self.bounds = [(1e-9, 150.), (1e-9, 10), (1e-9, 10), (1e-9, 10), (1e-9, 10), (1e-9, 10), (1e-9, 10)]
+        self.informed_bounds = [(20, 80), (1e-3, 1.), (1e-4, .1), (1e-2, 1), (.5, 5), (.5, 5), (1e-3, .5)]
+        self.num_reinit = 100
         self.n_jobs = -1
 
-    def _model(self, intensity, features, response_obs=None):
-        n_data = intensity.shape[0]
-        n_features = np.max(features, axis=0) + 1
-        feature0 = features[..., 0]
+    def functional(self, x, a, b, L, ell, H, c_1, c_2):
+        return S.rectified_logistic(
+            x, a, b, L, ell, H, eps=EPS
+        )
 
-        with numpyro.plate(site.n_response, self.n_response):
-            with numpyro.plate(site.n_features[0], n_features[0]):
-                # Uniform priors (maximum likelihood estimation)
-                a = numpyro.sample(
-                    site.a, dist.Uniform(0., 150.)
-                )
+    def cost_function(self, x, y_obs, a, b, L, ell, H, c_1, c_2):
+        mu = self.functional(x, a, b, L, ell, H, c_1, c_2)
+        beta = self.rate(mu, c_1, c_2)
+        alpha = self.concentration(mu, beta)
+        nll = -dist.Gamma(concentration=alpha, rate=beta).log_prob(y_obs)
+        return np.sum(nll)
 
-                b = numpyro.sample(site.b, dist.Uniform(0., 10.))
-
-                L = numpyro.sample(site.L, dist.Uniform(0., 10.))
-                ell = numpyro.sample(site.ell, dist.Uniform(0., 10.))
-                H = numpyro.sample(site.H, dist.Uniform(0., 10.))
-
-                c_1 = numpyro.sample(site.c_1, dist.Uniform(0., 10.))
-                c_2 = numpyro.sample(site.c_2, dist.Uniform(0., 10.))
-
-        with numpyro.plate(site.n_response, self.n_response):
-            with numpyro.plate(site.n_data, n_data):
-                # Model
-                mu = numpyro.deterministic(
-                    site.mu,
-                    S.rectified_logistic(
-                        x=intensity,
-                        a=a[feature0],
-                        b=b[feature0],
-                        L=L[feature0],
-                        ell=ell[feature0],
-                        H=H[feature0],
-                        eps=EPS
-                    )
-                )
-                beta = numpyro.deterministic(
-                    site.beta,
-                    self.rate(
-                        mu,
-                        c_1[feature0],
-                        c_2[feature0]
-                    )
-                )
-                alpha = numpyro.deterministic(
-                    site.alpha,
-                    self.concentration(mu, beta)
-                )
-
-                # Observation
-                numpyro.sample(
-                    site.obs,
-                    dist.Gamma(concentration=alpha, rate=beta),
-                    obs=response_obs
-                )
 
 
 class NelderMeadOptimization(BoundConstrainedOptimization):
@@ -246,7 +274,7 @@ class NelderMeadOptimization(BoundConstrainedOptimization):
         self.named_args = [site.a, site.b, site.L, site.ell, site.H]
         self.bounds = [(1e-9, 150.), (1e-9, 10), (1e-9, 10), (1e-9, 10), (1e-9, 10)]
         self.informed_bounds = [(20, 80), (1e-3, 1.), (1e-4, .1), (1e-2, 1), (.5, 5)]
-        self.num_reinit = 100
+        self.num_reinit = 10
         self.n_jobs = -1
 
     def functional(self, x, a, b, L, ell, H):
@@ -257,7 +285,6 @@ class NelderMeadOptimization(BoundConstrainedOptimization):
     def cost_function(self, x, y_obs, *args):
         y_pred = self.functional(x, *args)
         return np.sum((y_obs - y_pred) ** 2)
-
 
 # class RectifiedLogisticS50(GammaModel):
 #     NAME = "rectified_logistic_in_S50_parameterization"
